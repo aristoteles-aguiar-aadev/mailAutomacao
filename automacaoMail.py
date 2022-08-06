93d25b7312e4ac878f4e0b21e6133dc28e8c0638
aristoteaguiar <aristotelesouza@gmail.com>
2022-08-06 18:17:27 -0300

    mailAutomacao

---
+++ automacaoMail.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import win32com.client as win32
+
+# importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+# visualizar a base de dados
+pd.set_option('display.max.columns', None)
+print(tabela_vendas)
+
+print('-' * 50)
+# faturamento por loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
+print('-' * 50)
+# quantidade de produtos vendidos por loja
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
+
+print('-' * 50)
+# ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+print(ticket_medio)
+
+# enviar um email com o relatório
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'seu-email@gmail.com; seu-email@icloud.com'
+mail.Subject = 'Relatório de Vendas por Loja'
+mail.HTMLBody = f'''
+<p>Prezados,</p>
+
+<p>Segue o Relatório de Vendas por cada Loja.</p>
+
+<p>Faturamento:</p>
+{faturamento.to_html(formatters={'Valor Final': 'R$ {:,.2f}'.format})}
+
+<p>Quantidade Vendida:</p>
+{quantidade.to_html()}
+
+<p>Ticket Médio dos Produtos em cada Loja:</p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
+
+<p>Qualquer dúvida estou à disposição.</p>
+
+<p>Att.,</p>
+<p>Aristoteles Aguiar</p>
+'''
+
+mail.Send()
+
+print('Email Enviado')
